b5e662b77b3eda118e41913041ede97dae60453a
gabriel <gabriel@clarive.com>
2017-06-14 11:27:41 +0200

    fix improved functionality

--- init/docker-task.js
+++ init/docker-task.js
@@ -1,78 +1,81 @@
 var reg = require('cla/reg');
 
-reg.register('service.task.docker', {
+reg.register('service.docker.task', {
     name: 'Docker Task',
     icon: 'plugin/cla-docker-plugin/icon/logo-docker.svg',
     form: '/plugin/cla-docker-plugin/form/docker-task-form.js',
 
     handler: function(ctx, params) {
 
-        var regRunRemote = require('cla/reg');
-
+        var log = require('cla/log');
+        var reg = require('cla/reg');
         var errorsType = params.errors || 'fail';
+        var server = params.server;
         var command = '';
         var output = '';
+        var command = 'docker ';
+        var task = '';
+        var optionsTask = params.optionsTask || [];
+        var commandParameters = params.commandParameters || '';
+        var imageOrContainer = '';
+        var use = params.use || '';
+        var availableCommands = {
+            Image: ['run', 'create'],
+            Container: ['commit', 'exec']
+        };
 
-        var buildDockerCommand = function(params) {
-            var command = 'docker ';
-            var task = '';
-            var optionsTask = params.optionsTask || [];
-            var commandParameters = params.commandParameters || '';
-            var imageOrContainer = '';
-            var use = params.use || '';
-            var availableCommands = {
-                Image: ['run', 'create'],
-                Container: ['commit', 'exec']
-            };
+        var launchDockerCommand = function(server, command, errorsType, params) {
 
-            if (use == 'Image') {
-                task = params.taskImage || '';
-                if (params.imageVersion != '') {
-                    imageOrContainer = params.imageName + ':' + params.imageVersion || '';
-                } else {
-                    imageOrContainer = params.imageName || '';
+            output = reg.launch('service.scripting.remote', {
+                name: 'Docker Task',
+                config: {
+                    errors: errorsType,
+                    server: server,
+                    path: command,
+                    output_error: params.output_error,
+                    output_warn: params.output_warn,
+                    output_capture: params.output_capture,
+                    output_ok: params.output_ok,
+                    meta: params.meta,
+                    rc_ok: params.rcOk,
+                    rc_error: params.rcError,
+                    rc_warn: params.rcWarn
                 }
-            } else if (use == 'Container') {
-                task = params.taskContainer || '';
-                imageOrContainer = params.containerName || '';
-            } else {
-                task = params.taskNone || '';
-            }
-            command += task;
-
-            optionsTask.forEach(function(element) {
-                command += ' ' + element;
             });
 
-            if (use != 'Generic') {
-                command += ' ' + imageOrContainer;
-                if (availableCommands[use].indexOf(task) != -1) {
-                    command += ' ' + commandParameters;
-                }
-            }
-
-            return command;
+            return output;
         }
 
-        command = buildDockerCommand(params);
-
-        output = regRunRemote.launch('service.scripting.remote', {
-            name: 'Docker Task',
-            config: {
-                errors: errorsType,
-                server: params.server,
-                path: command,
-                output_error: params.output_error,
-                output_warn: params.output_warn,
-                output_capture: params.output_capture,
-                output_ok: params.output_ok,
-                meta: params.meta,
-                rc_ok: params.rcOk,
-                rc_error: params.rcError,
-                rc_warn: params.rcWarn
+        if (use == 'Image') {
+            task = params.taskImage || '';
+            if (params.imageVersion != '') {
+                imageOrContainer = params.imageName + ':' + params.imageVersion || '';
+            } else {
+                imageOrContainer = params.imageName || '';
             }
-        });
+        } else if (use == 'Container') {
+            task = params.taskContainer || '';
+            imageOrContainer = params.containerName || '';
+        } else {
+            task = params.taskNone || '';
+        }
+        command += task;
 
-        return output;
+        command = command + " " + optionsTask.join(" ");
+
+        if (use != 'Generic') {
+            command += ' ' + imageOrContainer;
+            if (availableCommands[use].indexOf(task) != -1) {
+                command += ' ' + commandParameters;
+            }
+        }
+
+        log.info("Launching command: " + command);
+
+        output = launchDockerCommand(sever, command, errorsType, params);
+
+        log.info("Command finished");
+
+        return output.response;
     }
 });
